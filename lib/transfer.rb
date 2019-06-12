f7d9ef8f6abc2f95d09cc158a1ceb4ee24f1a206
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-12 22:30:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,37 @@
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+
+  end
+
+  def execute_transaction
+    #sender and a receiver
+    #amount to transfer
+    #deduct the amount from the sender and add it to the receiver
+    if @sender.status != "open"
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    else
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    end
+
+end
+
+  def reverse_transfer
+    #reverse a transfer between two accounts
+  end
+
+
 end
